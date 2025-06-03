28b739223e2d324c04c75c8a3f6ec844eabb590b
beeniegit <kimorange0927@gmail.com>
2025-06-03 21:54:36 +0900

    250603 - g

--- NewStreamlitApplication/pages/차트_선.py
+++ NewStreamlitApplication/pages/차트_선.py
@@ -2,12 +2,16 @@ import streamlit as st
 import pandas as pd
 from 도구.STREAMLIT_도구 import *
 
+# 파일 업로드, 인스턴스 생성
 t = 파일_업로더_도구("파일 업로드")
 
+# 데이터프레임 생성
 df = t.데이터프레임_생성()
 
+# 조건부 데이터 추출
 df_request = df[['goals_overall', 'assists_overall']]
 
+# 데이터 시각화
 st.line_chart(df_request)
 
 
--- NewStreamlitApplication/pages/차트_파이플럿.py
+++ NewStreamlitApplication/pages/차트_파이플럿.py
@@ -3,19 +3,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from 도구.STREAMLIT_도구 import *
 
+# 파일 업로드, 인스턴스 생성
 t = 파일_업로더_도구("파일 업로드")
 
+# 데이터프레임 생성
 df = t.데이터프레임_생성()
 
+# 데이터 가져오기
 df_request = df["full_name"].head(5)
 y = df["goals_overall"].head(5)
-
 fig, ax = plt.subplots()
 
+# 데이터 원형 그래프로 만들기
 ax.pie(y, labels=df_request, autopct='%1.1f%%')
 
+# 찌그러짐 방지
 ax.axis('equal')
 
+# 데이터 시각화
 st.pyplot(fig)
 
 
